708c3d72e51b75225dfc658cb0742ce7e43e2186
TatsuyaSuzuki <tsuzu@recoro.co.jp>
2017-12-26 20:44:06 +0900

    no message

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("/var/www/html/sample/inc/sys_info.php");
-require_once("/var/www/html/sample/inc/config.php");
-require_once("/var/www/html/sample/inc/function.php");
-$mysqli = new mysqli($host, $username, $password, $dbname);
-if ($mysqli->connect_error) {
-	error_log($mysqli->connect_error);
-	exit;
-} ?>
--- inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "cZRF2vw72";
-$dbname = "365market";
-
-?>
